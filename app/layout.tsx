769122d82dd3703879e494c2cf751f41cae35719
liuziting <57311725+liu-ziting@users.noreply.github.com>
2023-12-28 15:11:03 +0800

    更新 layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,17 @@ export default function RootLayout({ children }: RootLayoutProps) {
           GeistMono.variable
         )}
       >
+         {/* 统计代码开始 */}
+        <script type="text/javascript">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "kcusqbn8ii");
+          `}
+        </script>
+        {/* 统计代码结束 */}
         <Toaster />
         <Providers
           attribute="class"
